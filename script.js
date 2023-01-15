6b665938b50bb8b8b53c8e3d0070ba4659c72e74
RegisLewis <regisaslewis@gmail.com>
2023-01-15 01:39:34 -0800

    table added

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const compute = document.getElementById("compute");
 compute.addEventListener("click", addHours);
+const addToday = document.getElementById("addToday");
+addToday.addEventListener("click", moveToTable);
 const hours = document.createElement("h2");
 hours.setAttribute("id", "hours");
+const h1 = document.getElementById("hr1");
+const m1 = document.getElementById("mn1");
+const h2 = document.getElementById("hr2");
+const m2 = document.getElementById("mn2");
 
 function autotab (input1, input2) {
   if (document.getElementById(input1).value.length == 2) {
@@ -24,11 +30,11 @@ function minutesToDecimals(hour, minutes) {
   }
   return `${hour}.${minutes.toFixed(0)}`;
 }
-function totalHours(hr1, mn1, hr2, mn2) {
-    hr1 = Number(document.getElementById("hr1").value);
-    hr2 = Number(document.getElementById("hr2").value);
-    mn1 = Number(document.getElementById("mn1").value);
-    mn2 = Number(document.getElementById("mn2").value);
+function totalTime(hr1, mn1, hr2, mn2) {
+    hr1 = Number(h1.value);
+    hr2 = Number(h2.value);
+    mn1 = Number(m1.value);
+    mn2 = Number(m2.value);
     if (hr1 <= 23 && hr2 <= 23 && mn1 <= 59 && mn2 <= 59) {
      if ( hr1 > hr2) {
       hr2 += 24;
@@ -39,14 +45,36 @@ function totalHours(hr1, mn1, hr2, mn2) {
 }
 
 function addHours() {
-    hours.innerText = totalHours();
+    hours.innerText = totalTime();
     document.getElementById("total").appendChild(hours);
 }
 
-document.getElementById("compute").addEventListener("keydown", function (event) {
+document.getElementById("compute").addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
+    document.getElementById("addToday").focus();
     return addHours();
   } else if ( event.key === "Backspace") {
     document.getElementById("mn2").focus();
   }
 })
+
+function reset() {
+  h1.value = "";
+  h2.value = "";
+  m1.value = "";
+  m2.value = "";
+  hours.innerText = "";
+  document.getElementById("hr1").focus();
+}
+
+function moveToTable() {
+  let table = document.getElementById("table");
+  let newRow = table.insertRow(-1); 
+  let newCell = newRow.insertCell(0);
+  let hoursText = document.createTextNode(`${h1.value}:${m1.value} - ${h2.value}:${m2.value}`);
+  newCell.appendChild(hoursText);
+  let newCell2 = newRow.insertCell(1);
+  let timeText = document.createTextNode(hours.innerText);
+  newCell2.appendChild(timeText);
+  reset();
+}
